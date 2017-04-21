2c407cea78c24141a104ecea8f0750e0393ef691
은선 <dmstjs930@naver.com>
2017-04-21 16:45:16 +0900

    update commentDiaryList

---
+++ src/service/CommentDiaryService.java
@@ -0,0 +1,29 @@
+package service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import repository.CommentDiaryDao;
+import vo.CommentDiary;
+
+@Component
+public class CommentDiaryService {
+	@Autowired
+	private CommentDiaryDao commentDiaryDao;
+	public void setCommentDiaryDao(CommentDiaryDao commentDiaryDao){
+		this.commentDiaryDao=commentDiaryDao;
+	}
+
+	//---------------------------------//
+	
+	public int insertCommentDiary(CommentDiary commentDiary) {
+		return commentDiaryDao.insertCommentDiary(commentDiary);
+	}
+	
+	public List<CommentDiary> selectCommentDiary(String myId){
+		return commentDiaryDao.selectCommentDiary(myId);
+	}
+
+}
